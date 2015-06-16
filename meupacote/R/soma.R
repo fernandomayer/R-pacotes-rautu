d34aff25ab20562d78e90dc9668da622328b9b57
Fernando Mayer <fernandomayer@gmail.com>
2015-06-16 16:07:42 -0300

    adiciona documentacao do roxygen2

--- meupacote/R/soma.R
+++ meupacote/R/soma.R
@@ -1,3 +1,35 @@
+##' @title Faz a Soma de Dois Números
+##' @name soma
+##'
+##' @description Uma (incrível) função que pega dois números e faz a
+##'     soma. Utilize este campo para descrever melhor o propósito de
+##'     sua função e o que ela é capaz de fazer.
+##'
+##' @param x Um número
+##' @param y Outro número
+##'
+##' @details Utilize este campo para escrever detalhes mais técnicos da
+##'     sua função (se necessário), ou para detalhar melhor como
+##'     utilizar determinados argumentos.
+##'
+##' @return A soma dos números \code{x} e \code{y}.
+##'
+##' @author Fernando Mayer
+##'
+##' @seealso \code{\link[base]{sum}}, \code{\link[base]{"+"}}
+##'
+##' @examples
+##' soma(2, 2)
+##'
+##' x <- 3
+##' y <- 4
+##' soma(x = x, y = y)
+##'
+##' ## Erro
+##' z <- "a"
+##' soma(x = x, y = z)
+##'
+##' @export
 soma <- function(x, y){
     x + y
 }
